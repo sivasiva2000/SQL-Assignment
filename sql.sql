9bf467b86d472d25534798137b86ce8d2b5bcb66
siva k <144662906+sivasiva2000@users.noreply.github.com>
2023-12-09 10:20:39 +0530

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -6,7 +6,7 @@ values(1,"Alfreds Futterkiste","Maria Anders","Obere Str. 57","Berin",12209,"Ger
 	  (2,"Ana Trujillo Emparedados y helados","Ana Trujilio","Avida, de la Constitución 2222","Mexico D.F",05021,"Mexico"), 
 	  (3,"Antonio Moreno Taqueria","Antonio Moreno","Mataderos 2312","Mexico D.F",05023,"Mexico");
 select * from Customers;
-drop table Orders;
+-- drop table Orders;
 create table Orders(OrderID int PRIMARY KEY,CustomerID int, EmployeeID int,orderDate date,shipperID int,FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID));
 INSERT INTO Orders (OrderID, CustomerID, EmployeeID, orderDate, shipperID)
 VALUES
@@ -14,7 +14,7 @@ VALUES
     (10309, 37, 3, '1996-09-19', 1),
     (10310, 77, 8, '1996-09-20', 2);
 
-drop table shippers;
+-- drop table shippers;
 create table Shipper(shipperID int,shipperName varchar(55),phone double);
 insert into Shipper(shipperID,shipperName,phone)
 values(1,"speed Express",5055559831),
